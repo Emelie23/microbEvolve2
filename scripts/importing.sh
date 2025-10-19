da5117e4e61471f963197c224f656d889855a224
rothya <ya.sha.roth@gmail.com>
2025-10-19 10:53:57 +0200

    importing script added

---
+++ scripts/importing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Actvate conda environment before running the shell script
+#I installed openpyxl into microbEvolve environment
+
+#Create directories microbEvolve and Data to store the data into SCRATCH
+mkdir -p "$HOME/microbEvolve2/data"
+
+#Create data directory variable
+data_dir="$HOME/microbEvolve2/data"
+
+#Import data
+wget -O "$data_dir/demux_paired_end.qza" \
+    "https://polybox.ethz.ch/index.php/s/zi5ZBrBwcn7SYof/download/demux-paired-end.qza"
+
+wget -O "$data_dir/metadata.xlsx" \
+    "https://polybox.ethz.ch/index.php/s/YQQggAqcQCApJmQ/download"
+
+# Export metadata into single .tsv files
+python3 <<EOF
+import pandas as pd
+
+df = pd.read_excel("$data_dir/metadata.xlsx", sheet_name="DataDictionary")
+df.to_csv("$data_dir/metadata_dictionary.tsv", sep="\t", index=False)
+
+df = pd.read_excel("$data_dir/metadata.xlsx", sheet_name="metadata_per_sample")
+df.to_csv("$data_dir/metadata_per_sample.tsv", sep="\t", index=False)
+
+df = pd.read_excel("$data_dir/metadata.xlsx", sheet_name="metadata_per_age")
+df.to_csv("$data_dir/metadata_per_age.tsv", sep="\t", index=False)
+
+EOF
