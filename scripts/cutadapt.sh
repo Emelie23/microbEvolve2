6955f40e8e89da59538dc1a834c537e60300dc17
flurin-sch <85993825+flurin-sch@users.noreply.github.com>
2025-10-27 11:49:29 +0100

    Add second primer variant to cutadapt.

--- scripts/cutadapt.sh
+++ scripts/cutadapt.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+set -e
+
 # This command trims primer sequences in the reads if they are found.
 # Sequences taken from https://astrobiomike.github.io/amplicon/dada2_workflow_ex
 # Untrimmed (no primers found) sequences are discarded; this allows us to see how many sequences contained primers.
 
-qiime cutadapt trim-paired \\
-    --i-demultiplexed-sequences data/raw/demux_paired_end.qza \\
-    --p-adapter-f ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC \\
-    --p-adapter-r ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC \\
-    --p-discard-untrimmed True \\
-    --o-trimmed-sequences data/raw/demux_paired_end_trimmed.qza
+qiime cutadapt trim-paired \
+    --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
+    --p-adapter-f ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC \
+    --p-adapter-r ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC \
+    --p-discard-untrimmed True \
+    --o-trimmed-sequences data/raw/demux_paired_end_trimmed-original-primers.qza
 
-qiime demux summarize \\
-    --i-data data/raw/demux_paired_end_trimmed.qza \\
-    --o-visualization data/processed/demux_paired_end_trimmed.qzv
+qiime demux summarize \
+    --i-data data/raw/demux_paired_end_trimmed-original-primers.qza \
+    --o-visualization data/processed/demux_paired_end_trimmed-original-primers.qzv
 
+qiime cutadapt trim-paired \
+    --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
+    --p-adapter-f ^GTGYCAGCMGCCGCGGTAA...ATTAGAWACCCBNGTAGTCC \
+    --p-adapter-r ^GGACTACNVGGGTWTCTAAT...TTACCGCGGCKGCTGRCAC \
+    --p-discard-untrimmed True \
+    --o-trimmed-sequences data/raw/demux_paired_end_trimmed-modified-primers.qza
+
+qiime demux summarize \
+    --i-data data/raw/demux_paired_end_trimmed-modified-primers.qza \
+    --o-visualization data/processed/demux_paired_end_trimmed-modified-primers.qzv
---
+++ scripts/cutadapt_submission.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=microbEvolve2_cutadapt
+#SBATCH --time=03:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+start_time=$(date +%s)
+
+source $HOME/.bashrc
+conda activate microbEvolve
+
+sh $HOME/microbEvolve2/scripts/cutadapt.sh
+
+# Calculate duration
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
+
+# Log job statistics
+echo "========== JOB STATISTICS =========="
+echo "End time: $(date)"
+echo "Duration: ${hours}h ${minutes}m ${seconds}s"
+echo "Job ID: $SLURM_JOB_ID"
+echo "=================================="
