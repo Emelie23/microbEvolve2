6a18cae9446dc1c2907cecf107b039d657b77548
flurin-sch <85993825+flurin-sch@users.noreply.github.com>
2025-10-28 18:39:21 +0100

    Only look for reverse complement of primers.

--- scripts/cutadapt.sh
+++ scripts/cutadapt.sh
@@ -2,14 +2,17 @@
 
 set -e
 
+export R_LIBS_USER=$CONDA_PREFIX/lib/R/library
+export R_HOME=$CONDA_PREFIX/lib/R
+
 # This command trims primer sequences in the reads if they are found.
 # Sequences taken from https://astrobiomike.github.io/amplicon/dada2_workflow_ex
 # Untrimmed (no primers found) sequences are discarded; this allows us to see how many sequences contained primers.
 
 qiime cutadapt trim-paired \
     --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
-    --p-adapter-f ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC \
-    --p-adapter-r ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC \
+    --p-adapter-f ATTAGAWACCCBDGTAGTCC \
+    --p-adapter-r TTACCGCGGCKGCTGGCAC \
     --p-discard-untrimmed True \
     --o-trimmed-sequences data/raw/demux_paired_end_trimmed-original-primers.qza
 
@@ -17,13 +20,13 @@ qiime demux summarize \
     --i-data data/raw/demux_paired_end_trimmed-original-primers.qza \
     --o-visualization data/processed/demux_paired_end_trimmed-original-primers.qzv
 
-qiime cutadapt trim-paired \
-    --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
-    --p-adapter-f ^GTGYCAGCMGCCGCGGTAA...ATTAGAWACCCBNGTAGTCC \
-    --p-adapter-r ^GGACTACNVGGGTWTCTAAT...TTACCGCGGCKGCTGRCAC \
-    --p-discard-untrimmed True \
-    --o-trimmed-sequences data/raw/demux_paired_end_trimmed-modified-primers.qza
+# qiime cutadapt trim-paired \
+#     --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
+#     --p-adapter-f ^GTGYCAGCMGCCGCGGTAA...ATTAGAWACCCBNGTAGTCC \
+#     --p-adapter-r ^GGACTACNVGGGTWTCTAAT...TTACCGCGGCKGCTGRCAC \
+#     --p-discard-untrimmed True \
+#     --o-trimmed-sequences data/raw/demux_paired_end_trimmed-modified-primers.qza
 
-qiime demux summarize \
-    --i-data data/raw/demux_paired_end_trimmed-modified-primers.qza \
-    --o-visualization data/processed/demux_paired_end_trimmed-modified-primers.qzv
+# qiime demux summarize \
+#     --i-data data/raw/demux_paired_end_trimmed-modified-primers.qza \
+#     --o-visualization data/processed/demux_paired_end_trimmed-modified-primers.qzv
