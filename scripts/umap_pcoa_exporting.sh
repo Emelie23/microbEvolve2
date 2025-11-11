e4ead7ec16d412a46471f3991b7b1d8c1288be3d
rothya <ya.sha.roth@gmail.com>
2025-11-11 14:47:27 +0100

    umap_pcoa exporting file added and created visualization file

---
+++ scripts/umap_pcoa_exporting.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Add logging function
+log() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+log "Starting UMAP and PCoA exporting script"
+
+data_dir="$HOME/microbEvolve2/data/raw"
+
+qiime tools export \
+  --input-path "$data_dir/umap_braycurtis.qza" \
+  --output-path "$data_dir/umap_pcoa_export/umap_braycurtis"
+
+qiime tools export \
+  --input-path "$data_dir/umap_jaccard.qza" \
+  --output-path "$data_dir/umap_pcoa_export/umap_jaccard"
+
+qiime tools export \
+  --input-path "$data_dir/pcoa_braycurtis.qza" \
+  --output-path "$data_dir/umap_pcoa_export/pcoa_braycurtis"
+
+qiime tools export \
+  --input-path "$data_dir/pcoa_jaccard.qza" \
+  --output-path "$data_dir/umap_pcoa_export/pcoa_jaccard"
+
+log "UMAP and PCoA script completed successfully!"
