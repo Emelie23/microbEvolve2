c2452c65aa0ba762de1881b116ec4d5bc034c304
Emelie23 <emelieg@gmx.ch>
2025-11-11 10:16:06 +0100

    final adpation

--- scripts/diversity_beta.sh
+++ scripts/diversity_beta.sh
@@ -4,11 +4,11 @@ data_dir="$HOME/microbEvolve2/data"
 
 #PERMANOVA test checking whether the observed categories are significantly grouped
 qiime diversity beta-group-significance \
-    --i-distance-matrix $data_dir/processed/boots-kmer-diversity/distance_matrices/braycurtis.qza \
+    --i-distance-matrix $data_dir/raw/boots_kmer_diversity/distance_matrices/braycurtis.qza \
     --m-metadata-file $data_dir/raw/metadata_per_sample.tsv \
     --m-metadata-column timepoint \
     --p-pairwise \
-    --o-visualization $data_dir/processed/boots-kmer-diversity/distance_matrices/braycurtis_significance \
+    --o-visualization $data_dir/processed/boots_kmer_diversity/distance_matrices/braycurtis_significance \
 
 
 
