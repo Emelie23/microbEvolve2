18a869c21f82ad291940a83cc2887ff8744475fd
Emelie23 <emelieg@gmx.ch>
2025-11-11 08:39:41 +0100

    adaption code: uniform saving

--- scripts/diversity_beta.sh
+++ scripts/diversity_beta.sh
@@ -4,20 +4,14 @@ data_dir="$HOME/microbEvolve2/data"
 
 #PERMANOVA test checking whether the observed categories are significantly grouped
 qiime diversity beta-group-significance \
-    --i-distance-matrix $data_dir/kmerizer-results/bray_curtis_distance_matrix.qza \
+    --i-distance-matrix $data_dir/processed/boots-kmer-diversity/distance_matrices/braycurtis.qza \
     --m-metadata-file $data_dir/raw/metadata_per_sample.tsv \
-    --m-metadata-column \
+    --m-metadata-column timepoint \
     --p-pairwise \
-    --o-visualization $data_dir/kmerizer-results/bray_curtis-env-significance.qzv
-    
+    --o-visualization $data_dir/processed/boots-kmer-diversity/distance_matrices/braycurtis_significance \
 
-qiime diversity beta-group-significance \
-    --i-distance-matrix $data_dir/core-metrics-results/bray_curtis_distance_matrix.qza \
-    --m-metadata-file $data_dir/raw/metadata_per_sample.tsv \
-    --m-metadata-column \
-    --p-pairwise \
-    --o-visualization $data_dir/core-metrics-results/bray_curtis-env-significance.qzv
-    
-    
-    
+
+
+
+
  
