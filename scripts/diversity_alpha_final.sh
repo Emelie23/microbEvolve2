2d0798b983b1dab229234a1f730ebd407cd22392
rothya <ya.sha.roth@gmail.com>
2025-11-19 10:42:32 +0100

    adapted alpha diversity

--- scripts/diversity_alpha_final.sh
+++ scripts/diversity_alpha_final.sh
@@ -35,16 +35,11 @@ log "Starting bootstrapping..."
     --output-dir $data_dir/raw/boots_kmer_diversity
 
 log "Bootstrapping completed"
-log "Starting collapsing (taxonomy)..."
 
-! qiime taxa collapse \
-  --i-table $data_dir/raw/dada2_table.qza \
-  --i-taxonomy $data_dir/raw/taxonomy_unweighted.qza \
-  --p-level 6 \
-  --o-collapsed-table $data_dir/raw/collapsed_table_l6
-
-log "Collapsing completed"
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity $data_dir/raw/boots_kmer_diversity/alpha_diversities/shannon_vector.qza \
+  --m-metadata-file $data_dir/metadata.tsv \
+  --o-visualization $data_dir/processed/shannon-group-significance.qzv
   
 log "Diversity script completed successfully!"
-  
-  
+
