850d461a1074292ddfe9aa5b5fb94b3a1e5ce0e1
rothya <ya.sha.roth@gmail.com>
2025-12-06 15:54:55 +0100

    rearranged the code in cutadapt that it makes sense in the notebook

--- scripts/src/cutadapt.sh
+++ scripts/src/cutadapt.sh
@@ -9,24 +9,30 @@ export R_HOME=$CONDA_PREFIX/lib/R
 # Sequences taken from https://astrobiomike.github.io/amplicon/dada2_workflow_ex
 # Untrimmed (no primers found) sequences are discarded; this allows us to see how many sequences contained primers.
 
+data_dir="../data"
+
+# Initial trimming attempt (original and modified primers)
 qiime cutadapt trim-paired \
-    --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
+    --i-demultiplexed-sequences $data_dir/raw/demux_paired_end.qza \
+    --p-adapter-f ^GTGYCAGCMGCCGCGGTAA...ATTAGAWACCCBNGTAGTCC \
+    --p-adapter-r ^GGACTACNVGGGTWTCTAAT...TTACCGCGGCKGCTGRCAC \
+    --p-discard-untrimmed True \
+    --o-trimmed-sequences $data_dir/raw/demux_paired_end_trimmed-modified-primers.qza
+
+qiime demux summarize \
+    --i-data $data_dir/raw/demux_paired_end_trimmed-modified-primers.qza \
+    --o-visualization $data_dir/processed/demux_paired_end_trimmed-modified-primers.qzv
+
+# Identifying and Trimming Read-Through (Successful Strategy)
+qiime cutadapt trim-paired \
+    --i-demultiplexed-sequences $data_dir/raw/demux_paired_end.qza \
     --p-adapter-f ATTAGAWACCCBDGTAGTCC \
     --p-adapter-r TTACCGCGGCKGCTGGCAC \
     --p-discard-untrimmed True \
-    --o-trimmed-sequences data/raw/demux_paired_end_trimmed-original-primers.qza
+    --o-trimmed-sequences $data_dir/raw/demux_paired_end_trimmed-original-primers.qza
 
 qiime demux summarize \
-    --i-data data/raw/demux_paired_end_trimmed-original-primers.qza \
-    --o-visualization data/processed/demux_paired_end_trimmed-original-primers.qzv
+    --i-data $data_dir/raw/demux_paired_end_trimmed-original-primers.qza \
+    --o-visualization $data_dir/processed/demux_paired_end_trimmed-original-primers.qzv
 
-# qiime cutadapt trim-paired \
-#     --i-demultiplexed-sequences data/raw/demux_paired_end.qza \
-#     --p-adapter-f ^GTGYCAGCMGCCGCGGTAA...ATTAGAWACCCBNGTAGTCC \
-#     --p-adapter-r ^GGACTACNVGGGTWTCTAAT...TTACCGCGGCKGCTGRCAC \
-#     --p-discard-untrimmed True \
-#     --o-trimmed-sequences data/raw/demux_paired_end_trimmed-modified-primers.qza
 
-# qiime demux summarize \
-#     --i-data data/raw/demux_paired_end_trimmed-modified-primers.qza \
-#     --o-visualization data/processed/demux_paired_end_trimmed-modified-primers.qzv
